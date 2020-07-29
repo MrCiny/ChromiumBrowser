2822612bd4076abcbb3690baabeb480e8a31adca
niks.janeks@gmail.com <niks.janeks@gmail.com>
2020-07-29 16:51:56 +0300

    Add project files.

---
+++ Browser.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ChromiumBrowser
+{
+    partial class Browser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Browser));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonHome = new System.Windows.Forms.ToolStripButton();
+            this.toolStripTextBoxAddressBar = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripButtonBack = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonForward = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonReload = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonAddTab = new System.Windows.Forms.ToolStripButton();
+            this.BrowserTabs = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.toolStrip1.SuspendLayout();
+            this.BrowserTabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonHome,
+            this.toolStripTextBoxAddressBar,
+            this.toolStripButtonBack,
+            this.toolStripButtonForward,
+            this.toolStripButtonReload,
+            this.toolStripButtonAddTab});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonHome
+            // 
+            this.toolStripButtonHome.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonHome.Name = "toolStripButtonHome";
+            this.toolStripButtonHome.Size = new System.Drawing.Size(44, 22);
+            this.toolStripButtonHome.Text = "Home";
+            this.toolStripButtonHome.Click += new System.EventHandler(this.toolStripButtonGo_Click);
+            // 
+            // toolStripTextBoxAddressBar
+            // 
+            this.toolStripTextBoxAddressBar.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBoxAddressBar.Name = "toolStripTextBoxAddressBar";
+            this.toolStripTextBoxAddressBar.Size = new System.Drawing.Size(400, 25);
+            this.toolStripTextBoxAddressBar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBoxAddressBar_KeyDown);
+            // 
+            // toolStripButtonBack
+            // 
+            this.toolStripButtonBack.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonBack.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonBack.Image")));
+            this.toolStripButtonBack.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonBack.Name = "toolStripButtonBack";
+            this.toolStripButtonBack.Size = new System.Drawing.Size(36, 22);
+            this.toolStripButtonBack.Text = "Back";
+            this.toolStripButtonBack.Click += new System.EventHandler(this.toolStripButtonBack_Click);
+            // 
+            // toolStripButtonForward
+            // 
+            this.toolStripButtonForward.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonForward.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonForward.Image")));
+            this.toolStripButtonForward.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonForward.Name = "toolStripButtonForward";
+            this.toolStripButtonForward.Size = new System.Drawing.Size(54, 22);
+            this.toolStripButtonForward.Text = "Forward";
+            this.toolStripButtonForward.Click += new System.EventHandler(this.toolStripButtonForward_Click);
+            // 
+            // toolStripButtonReload
+            // 
+            this.toolStripButtonReload.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonReload.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonReload.Image")));
+            this.toolStripButtonReload.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonReload.Name = "toolStripButtonReload";
+            this.toolStripButtonReload.Size = new System.Drawing.Size(47, 22);
+            this.toolStripButtonReload.Text = "Reload";
+            this.toolStripButtonReload.Click += new System.EventHandler(this.toolStripButtonReload_Click);
+            // 
+            // toolStripButtonAddTab
+            // 
+            this.toolStripButtonAddTab.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonAddTab.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonAddTab.Image")));
+            this.toolStripButtonAddTab.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonAddTab.Name = "toolStripButtonAddTab";
+            this.toolStripButtonAddTab.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonAddTab.Text = "+";
+            this.toolStripButtonAddTab.Click += new System.EventHandler(this.toolStripButtonAddTab_Click);
+            // 
+            // BrowserTabs
+            // 
+            this.BrowserTabs.Controls.Add(this.tabPage1);
+            this.BrowserTabs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BrowserTabs.Location = new System.Drawing.Point(0, 25);
+            this.BrowserTabs.Name = "BrowserTabs";
+            this.BrowserTabs.SelectedIndex = 0;
+            this.BrowserTabs.Size = new System.Drawing.Size(800, 425);
+            this.BrowserTabs.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 399);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Browser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BrowserTabs);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Browser";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.BrowserTabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonHome;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBoxAddressBar;
+        private System.Windows.Forms.ToolStripButton toolStripButtonBack;
+        private System.Windows.Forms.ToolStripButton toolStripButtonForward;
+        private System.Windows.Forms.ToolStripButton toolStripButtonReload;
+        private System.Windows.Forms.ToolStripButton toolStripButtonAddTab;
+        private System.Windows.Forms.TabControl BrowserTabs;
+        private System.Windows.Forms.TabPage tabPage1;
+    }
+}
+
---
+++ Browser.cs
@@ -0,0 +1,134 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChromiumBrowser
+{
+    public partial class Browser : Form
+    {
+        private ChromiumWebBrowser browser;
+        int tabCount = 0;
+
+        public Browser()
+        {
+            InitializeComponent();
+            InitializeBrowser();
+            InitializeForm();
+        }
+
+        private void InitializeForm()
+        {
+
+            BrowserTabs.Height = ClientRectangle.Height - 25;
+        }
+
+        private void InitializeBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            AddBrowser();
+        }
+
+        private void AddBrowser()
+        {
+            browser = new ChromiumWebBrowser("https://datorium.eu/");
+            browser.Dock = DockStyle.Fill;
+            BrowserTabs.TabPages[tabCount].Controls.Add(browser);
+            browser.AddressChanged += Browser_AddressChanged;
+            browser.TitleChanged += Browser_TitleChanged;
+            tabCount++;
+        }
+
+        private void NewTab()
+        {
+            var newTabPage = new TabPage();
+
+            newTabPage.Text = "New Tab";
+            BrowserTabs.TabPages.Add(newTabPage);
+            BrowserTabs.SelectedTab = newTabPage;
+            AddBrowser();
+        }
+
+        private void toolStripButtonGo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                browser.Load("https://google.com");
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void toolStripButtonBack_Click(object sender, EventArgs e)
+        {
+            browser.Back();
+        }
+
+        private void toolStripButtonForward_Click(object sender, EventArgs e)
+        {
+            browser.Forward();
+        }
+
+        private void Browser_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            var selectedBrowser = (ChromiumWebBrowser)sender;
+            this.Invoke(new MethodInvoker(() =>
+            {
+                toolStripTextBoxAddressBar.Text = e.Address;
+            }));
+        }
+
+        private void toolStripTextBoxAddressBar_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                Navigate(toolStripTextBoxAddressBar.Text);
+            }
+        }
+
+        private void toolStripButtonReload_Click(object sender, EventArgs e)
+        {
+            browser.Reload();
+        }
+
+        private void Navigate(string address)
+        {
+            try
+            {
+                var selectedBrowser = (ChromiumWebBrowser)BrowserTabs.SelectedTab.Controls[0];
+
+                selectedBrowser.Load(address);
+                selectedBrowser.TitleChanged += Browser_TitleChanged;
+                
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void toolStripButtonAddTab_Click(object sender, EventArgs e)
+        {
+            NewTab();
+        }
+
+        private void Browser_TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            var selectedBrowser = (ChromiumWebBrowser)sender;
+            this.Invoke(new MethodInvoker(() =>
+            {
+                selectedBrowser.Parent.Text = e.Title;
+            }));
+        }
+    }
+}
